156ea60863d3bb620dddd01215a6ea99883ac908
Methawee A <merimo.mayjiiex@hotmail.com>
2017-05-09 18:11:29 +0700

    update with customer.php

--- js/connected.js
+++ js/connected.js
@@ -1,37 +1,60 @@
 /**
  * Created by methawee on 5/4/2017 AD.
  */
-$(document).ready(function () {
-        $("#submit").click(function () {
-            console.log("check submit");
-            var first_name = $("#first_name").val();
-            // console.log(first_name);
-            var last_name = $("#last_name").val();
-            var address = $("#address").val();
-            var email = $("#email").val();
-            var password = $("#password").val();
-            
-            var request = $.ajax({
-                method: "POST",
-                url: "api.kamontat.me",
-                dataType: "json",
-                data: {
-                    "action": "insert_customer",
-                    "first_s": first_name,
-                    "last_s": last_name,
-                    "address_s": address,
-                    "email_s": email,
-                    "password": password
-                }
-            });
-            request.done(function (response, status, xhr) {
-                console.info(xhr);
-                // alert("Is success: " + response.success + "\n response: " + JSON.stringify(response));
-            });
-            request.fail(function (xhr, status, error) {
-                // alert(error);
-                console.error(xhr);
-            });
-            window.parent.location = "booking.html";
-        });
-});
+
+function insert_customer() {
+    console.log("hi! i'm ur new customer ♡");
+    $.ajax({
+        type: 'post',
+        url: 'customer.php',
+        data: {
+            'key': 'insert_customer',
+            'first_name': document.getElementById('first_name').value,
+            'last_name': document.getElementById('last_name').value,
+            'address': document.getElementById('address').value,
+            'email': document.getElementById('email').value,
+            'password': document.getElementById('password').value
+        },
+        success: function (data) {
+            console.log("insert customer is success: " + data);
+        }
+    });
+}
+
+function login() {
+    console.log("i'm your old customer! ♡");
+    $.ajax({
+        type: 'post',
+        url: 'customer.php',
+        data: {
+            'key': 'login',
+            'input_email': document.getElementById('InputEmail').value,
+            'input_password': document.getElementById('InputPassword').value
+        },
+        success: function (data) {
+            console.log("login is success: " + data);
+        }
+    });
+}
+
+function insert_payment() {
+    console.log("payment method ♡");
+    $.ajax({
+        type: 'post',
+        url: 'customer.php',
+        data: {
+            'key': 'payment',
+            'nameOnCard': document.getElementById('nameOnCard').value,
+            'creditCardNo': document.getElementById('creditCardNo').value,
+            'expireDate': document.getElementById('expireDate').value
+        },
+        success: function (data) {
+            console.log("payment is success: " + data);
+        }
+    });
+}
+
+function load_booking() {
+    /* this is work, but doesn't call yet */
+    window.location.href = 'booking.html';
+}
--- js/passCheck.js
+++ js/passCheck.js
@@ -1,30 +1,27 @@
+document.addEventListener("DOMContentLoaded", function () {
 
-document.addEventListener("DOMContentLoaded", function() {
 
-
-    var checkPassword = function(str)
-    {
+    var checkPassword = function (str) {
         var re = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{6,}$/;
         return re.test(str);
     };
 
-    var checkForm = function(e)
-    {
-        if(this.username.value == "") {
+    var checkForm = function (e) {
+        if (this.username.value == "") {
             alert("Error: Username cannot be blank!");
             this.username.focus();
             e.preventDefault(); // equivalent to return false
             return;
         }
         re = /^\w+$/;
-        if(!re.test(this.username.value)) {
+        if (!re.test(this.username.value)) {
             alert("Error: name must contain only letters!");
             this.username.focus();
             e.preventDefault();
             return;
         }
-        if(this.pwd1.value != "" && this.pwd1.value == this.pwd2.value) {
-            if(!checkPassword(this.pwd1.value)) {
+        if (this.pwd1.value != "" && this.pwd1.value == this.pwd2.value) {
+            if (!checkPassword(this.pwd1.value)) {
                 alert("The password you have entered is not valid!");
                 this.pwd1.focus();
                 e.preventDefault();
@@ -42,13 +39,12 @@ document.addEventListener("DOMContentLoaded", function() {
     var myForm = document.getElementById("myForm");
     myForm.addEventListener("submit", checkForm, true);
 
-    var supports_input_validity = function()
-    {
+    var supports_input_validity = function () {
         var i = document.createElement("input");
         return "setCustomValidity" in i;
     }
 
-    if(supports_input_validity()) {
+    if (supports_input_validity()) {
         var fnInput = document.getElementById("field_fn");
         fnInput.setCustomValidity(fnInput.title);
 
@@ -60,17 +56,17 @@ document.addEventListener("DOMContentLoaded", function() {
 
         var pwd2Input = document.getElementById("field_pwd2");
 
-        fnInput.addEventListener("keyup", function() {
+        fnInput.addEventListener("keyup", function () {
             fnInput.setCustomValidity(this.validity.patternMismatch ? fnInput.title : "");
         }, false);
 
-        lnInput.addEventListener("keyup", function() {
+        lnInput.addEventListener("keyup", function () {
             lnInput.setCustomValidity(this.validity.patternMismatch ? lnInput.title : "");
         }, false);
 
-        pwd1Input.addEventListener("keyup", function() {
+        pwd1Input.addEventListener("keyup", function () {
             this.setCustomValidity(this.validity.patternMismatch ? pwd1Input.title : "");
-            if(this.checkValidity()) {
+            if (this.checkValidity()) {
                 pwd2Input.pattern = this.value;
                 pwd2Input.setCustomValidity(pwd2Input.title);
             } else {
@@ -79,7 +75,7 @@ document.addEventListener("DOMContentLoaded", function() {
             }
         }, false);
 
-        pwd2Input.addEventListener("keyup", function() {
+        pwd2Input.addEventListener("keyup", function () {
             this.setCustomValidity(this.validity.patternMismatch ? pwd2Input.title : "");
         }, false);
 
